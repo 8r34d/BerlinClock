41eb2dec5c92c3db3249eba06ffd6bed66455cf9
dean spencer <dean_spencer@yahoo.co.uk>
2014-03-11 16:25:16 +0000

    BerlinClock implementation and tests

--- src/main/java/com/inkglobal/techtest/BerlinClock.java
+++ src/main/java/com/inkglobal/techtest/BerlinClock.java
@@ -1,4 +1,113 @@
 package com.inkglobal.techtest;
 
+import org.apache.commons.lang3.StringUtils;;
+
 public class BerlinClock {
+
+    private static final String OFF = "O";
+    private static final String RED = "R";
+    private static final String YELLOW = "Y";
+    private static final String NEWLINE = "\n";
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public String getTime() {
+        StringBuilder s = new StringBuilder();
+        s.append(getSeconds());
+        s.append(getHours());
+        s.append(getMinutes());
+        return s.toString();
+    }
+
+    public void setTime(String t) {
+        String[] pieces = t.split(":");
+        setHours(pieces[0]);
+        setMinutes(pieces[1]);
+        setSeconds(pieces[2]);
+    }
+
+    public String getSeconds() {
+        if(seconds % 2 == 0) {
+            return YELLOW + NEWLINE;
+        } else {
+            return OFF + NEWLINE;
+        }
+    }
+
+    public String getHours() {
+        int quotient = hours / 5;
+        int remainder = hours % 5;
+        StringBuilder s = new StringBuilder();
+            s.append(rowOfFour(quotient));
+            s.append(NEWLINE);
+            s.append(rowOfFour(remainder));
+            s.append(NEWLINE);
+        return s.toString();
+    }
+
+    public String getMinutes() {
+        int quotient = minutes / 5;
+        int remainder = minutes % 5;
+        StringBuilder s = new StringBuilder();
+            s.append(rowOfEleven(quotient));
+            s.append(NEWLINE);
+            s.append(rowOfFour(remainder).replace(RED, YELLOW));
+            s.append(NEWLINE);
+        return s.toString();
+    }
+
+    private void setSeconds(String s) {
+        int i = Integer.parseInt(s);
+        if(i >= 0 && i <= 59) {
+            seconds = i;
+        } else {
+            throw new IllegalArgumentException(
+                    "seconds must be between 0 and 59"
+            );
+        }
+    }
+
+    private void setMinutes(String s) {
+        int i = Integer.parseInt(s);
+        if(i >= 0 && i <= 59) {
+            minutes = i;
+        } else {
+            throw new IllegalArgumentException(
+                    "minutes must be between 0 and 59"
+            );
+        }
+    }
+
+    private void setHours(String s) {
+        int i = Integer.parseInt(s);
+        if(i >= 0 && i <= 24) {
+            hours = i;
+        } else {
+            throw new IllegalArgumentException(
+                    "hours must be between 0 and 24"
+            );
+        }
+    }
+
+    private String rowOfFour(int value) {
+        String[] off = {OFF, OFF, OFF, OFF};
+        for(int i = 0; i < value; i++) {
+            off[i] = RED;
+        }
+        return StringUtils.join(off, "");
+    }
+
+    private String rowOfEleven(int value) {
+        String[] off = {OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF};
+        for(int i = 0; i < value; i++) {
+            if((i + 1) % 3 == 0) {
+                off[i] = RED;
+            } else {
+                off[i] = YELLOW;
+            }
+        }
+        return StringUtils.join(off, "");
+    }
+
 }
--- src/test/java/com/inkglobal/techtest/BerlinClockTest.java
+++ src/test/java/com/inkglobal/techtest/BerlinClockTest.java
@@ -1,6 +1,155 @@
 package com.inkglobal.techtest;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BerlinClockTest {
+
+    private BerlinClock berlinClock;
+
+    @Before
+    public void setup() {
+        berlinClock = new BerlinClock();
+    }
+
+    @Test
+    public void testTwentyFourZeroZero() {
+        String s = "24:00:00";
+        berlinClock.setTime(s);
+        String g = "Y\nRRRR\nRRRR\nOOOOOOOOOOO\nOOOO\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getTime(),
+                berlinClock.getTime().equals(g)
+        );
+    }
+
+    @Test
+    public void testTwentyThreeFiftyNineFiftyNine() {
+        String s = "23:59:59";
+        berlinClock.setTime(s);
+        String g = "O\nRRRR\nRRRO\nYYRYYRYYRYY\nYYYY\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getTime(),
+                berlinClock.getTime().equals(g)
+        );
+    }
+
+    @Test
+    public void testThirteenSeventeenOne() {
+        String s = "13:17:01";
+        berlinClock.setTime(s);
+        String g = "O\nRROO\nRRRO\nYYROOOOOOOO\nYYOO\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getTime(),
+                berlinClock.getTime().equals(g)
+        );
+    }
+
+    @Test
+    public void testZeroZeroZero() {
+        String s = "00:00:00";
+        berlinClock.setTime(s);
+        String g = "Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getTime(),
+                berlinClock.getTime().equals(g)
+        );
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testNegativeSeconds() {
+        berlinClock.setTime("11:22:-33");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testSixtySeconds() {
+        berlinClock.setTime("11:22:60");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testNegativeMinutes() {
+        berlinClock.setTime("11:-22:33");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testSixtyMinutes() {
+        berlinClock.setTime("11:60:33");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testNegativeHours() {
+        berlinClock.setTime("-11:22:33");
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testTwentyFiveHours() {
+        berlinClock.setTime("25:22:33");
+    }
+
+    @Test
+    public void testSecondsEven() {
+        String s = "11:22:00";
+        berlinClock.setTime(s);
+        String g = "Y\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getSeconds(),
+                berlinClock.getSeconds().equals(g)
+        );
+    }
+
+    @Test
+    public void testSecondsOdd() {
+        String s = "11:22:01";
+        berlinClock.setTime(s);
+        String g = "O\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getSeconds(),
+                berlinClock.getSeconds().equals(g)
+        );
+    }
+
+    @Test
+    public void testZeroMinutes() {
+        String s = "00:00:00";
+        berlinClock.setTime(s);
+        String g = "OOOOOOOOOOO\nOOOO\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getMinutes(),
+                berlinClock.getMinutes().equals(g)
+        );
+    }
+
+    @Test
+    public void testFiftyNineMinutes() {
+        String s = "00:59:00";
+        berlinClock.setTime(s);
+        String g = "YYRYYRYYRYY\nYYYY\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getMinutes(),
+                berlinClock.getMinutes().equals(g)
+        );
+    }
+
+    @Test
+    public void testZeroHours() {
+        String s = "00:00:00";
+        berlinClock.setTime(s);
+        String g = "OOOO\nOOOO\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getHours(),
+                berlinClock.getHours().equals(g)
+        );
+    }
+
+    @Test
+    public void testTwentyFourHours() {
+        String s = "24:00:00";
+        berlinClock.setTime(s);
+        String g = "RRRR\nRRRR\n";
+        Assert.assertTrue(
+                "expected: " + g + " actual: " + berlinClock.getHours(),
+                berlinClock.getHours().equals(g)
+        );
+    }
 }
